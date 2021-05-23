01f87574347c3a6cc02b4a2cb7d6209daa48efb2
wangtao <1182792703@qq.com>
2021-05-23 09:46:49 +0800

    节点前、节点后、节点追加、修改节点的父亲节点功能

--- src/dom.js
+++ src/dom.js
@@ -9,5 +9,16 @@ window.dom = {
     // }
     after(node, node2){
         node.parentNode.insertBefore(node2, node.nextSibling)
-    }
+    },
+    before(node, node2){
+        node.parentNode.insertBefore(node2, node)
+    },
+    append(parent, node){
+        parent.appendChild(node)
+    },
+    // 修改节点的父级关系
+    wrap(node, parent){
+        dom.before(node, parent);
+        dom.append(parent, node)
+    },
 };
--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ const div = dom.create("<div>newDiv</div>");
 console.log(div);
 
 dom.after(test, div)
+
+
+const div3 = dom.create("<div>this is test's parent node</div>");
+dom.wrap(test, div3)
